704e23973a3431fe92d583fc5e0a50655541bf75
cjcroldan <92191056+cjcroldan@users.noreply.github.com>
2022-09-07 08:57:35 +0800

    Add files via upload

---
+++ Q1Ex1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex1;
+
+/**
+ *
+ * @author TAU
+ */
+public class Q1Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String subj1 = "Mathematics 4";
+        int mtgs1 = 4;
+        double hrs1 = 3;
+        
+        String subj2 = "Computer Science 4";
+        int mtgs2 = 3;
+        double hrs2 = 2.25;
+        
+        String subj3 = "Physical Education 5";
+        int mtgs3 = 2;
+        double hrs3 = 1.5;
+        // TODO code application logic here
+    }
+    
+}
